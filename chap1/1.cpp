7bfd966da076ee94d33bdae2924a2cdb5e2b999e
yanchong <blackpeng@outlook.com>
2021-02-06 10:13:22 +0800

    add chap1 and 1.cpp

---
+++ chap1/1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void print1();
+void print2();
+void print3();
+
+int main()
+{
+	print1();
+	print2();
+	print3();
+	
+	return 0;
+}
+
+void print1()
+{
+	cout << "Three blind mouse" << endl;
+	cout << "Three blind mouse" << endl;
+}
+
+void print2()
+{
+	cout << "See how they run" << endl;
+}
+
+void print3()
+{
+	cout << "See how they run" << endl;
+}
+
